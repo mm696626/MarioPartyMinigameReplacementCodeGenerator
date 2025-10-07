3c2ef4d01d834dce57daefb1f1e6f946f407be0c
Matt McCullough <mmccullough6899@gmail.com>
2025-10-07 19:46:30 -0400

    Remove Story and Extra games and move Mic games to separate categories

--- src/constants/MinigameCategoryConstants.java
+++ src/constants/MinigameCategoryConstants.java
@@ -3,19 +3,19 @@ package constants;
 public class MinigameCategoryConstants {
 
     public static final int FOUR_PLAYER_MINIGAME = 0;
-    public static final int ONE_V_THREE_MINIGAME = 1;
-    public static final int TWO_V_TWO_MINIGAME = 2;
-    public static final int BATTLE_MINIGAME = 3;
-    public static final int DUEL_MINIGAME = 4;
-    public static final int BOWSER_MINIGAME = 5;
-    public static final int DK_MINIGAME = 6;
-    public static final int STORY_MINIGAME = 7;
-    public static final int EXTRA_MINIGAME = 8; //using the same category for Rare
-    public static final int EIGHT_PLAYER_MINIGAME = 9;
-    public static final int ONE_PLAYER_MINIGAME = 10;
+    public static final int FOUR_PLAYER_MIC_MINIGAME = 1;
+    public static final int FOUR_PLAYER_DUEL_MINIGAME = 2; //literally only for the shared 4 Player/Duel minigames in 8
+    public static final int ONE_V_THREE_MINIGAME = 3;
+    public static final int ONE_V_THREE_MIC_MINIGAME = 4;
+    public static final int TWO_V_TWO_MINIGAME = 5;
+    public static final int TWO_V_TWO_DUEL_MINIGAME = 6; //literally only for the shared 2v2/Duel minigames in 8
+    public static final int BATTLE_MINIGAME = 7;
+    public static final int BATTLE_DUEL_MINIGAME = 8; //literally only for the shared Battle/Duel minigames in 8
+    public static final int DUEL_MINIGAME = 9;
+    public static final int BOWSER_MINIGAME = 10;
     public static final int ONE_PLAYER_BOWSER_MINIGAME = 11;
-    public static final int ONE_PLAYER_DK_MINIGAME = 12;
-    public static final int FOUR_PLAYER_DUEL_MINIGAME = 13; //literally only for the shared 4 Player/Duel minigames in 8
-    public static final int TWO_V_TWO_DUEL_MINIGAME = 14; //literally only for the shared 2v2/Duel minigames in 8
-    public static final int BATTLE_DUEL_MINIGAME = 15; //literally only for the shared Battle/Duel minigames in 8
+    public static final int DK_MINIGAME = 12;
+    public static final int ONE_PLAYER_DK_MINIGAME = 13;
+    public static final int ONE_PLAYER_MINIGAME = 14;
+    public static final int EIGHT_PLAYER_MINIGAME = 15;
 }
--- src/constants/MinigameCategoryUIConstants.java
+++ src/constants/MinigameCategoryUIConstants.java
@@ -8,40 +8,40 @@ public class MinigameCategoryUIConstants {
     public static final String[] CATEGORY_NAMES = {
             "All",
             "4 Player",
+            "4 Player Mic",
+            "4 Player/Duel",
             "1v3",
+            "1v3 Mic",
             "2v2",
+            "2v2/Duel",
             "Battle",
+            "Battle/Duel",
             "Duel",
             "Bowser",
-            "DK",
-            "Story",
-            "Extra",
-            "8 Player",
-            "1 Player",
             "1 Player Bowser",
+            "DK",
             "1 Player DK",
-            "4 Player/Duel",
-            "2v2/Duel",
-            "Battle/Duel"
+            "1 Player",
+            "8 Player"
     };
 
     public static final Map<Integer, String> MINIGAME_CATEGORY_MAP = new HashMap<>() {{
         put(-1, "All");
         put(0, "4 Player");
-        put(1, "1v3");
-        put(2, "2v2");
-        put(3, "Battle");
-        put(4, "Duel");
-        put(5, "Bowser");
-        put(6, "DK");
-        put(7, "Story");
-        put(8, "Extra");
-        put(9, "8 Player");
-        put(10, "1 Player");
+        put(1, "4 Player Mic");
+        put(2, "4 Player/Duel");
+        put(3, "1v3");
+        put(4, "1v3 Mic");
+        put(5, "2v2");
+        put(6, "2v2/Duel");
+        put(7, "Battle");
+        put(8, "Battle/Duel");
+        put(9, "Duel");
+        put(10, "Bowser");
         put(11, "1 Player Bowser");
-        put(12, "1 Player DK");
-        put(13, "4 Player/Duel");
-        put(14, "2v2/Duel");
-        put(15, "Battle/Duel");
+        put(12, "DK");
+        put(13, "1 Player DK");
+        put(14, "1 Player");
+        put(15, "8 Player");
     }};
 }
